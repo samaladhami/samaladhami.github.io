21be2f94cd9af6e733a1294734adaf0b881a085f
samer <samaladhami@gmail.com>
2016-11-12 22:45:59 -0600

    animations

---
+++ directives/homeDirective.js
@@ -0,0 +1,75 @@
+angular.module('portfolio')
+.directive('homeDirective' , function() {
+
+  return{
+    restract: 'E'
+    ,templateUrl: 'templates/homeDirective.html'
+    ,controller: 'homeCtrl'
+    ,link: function() {
+
+      let marginCounter = 0;
+      let sideBarMarginCounter = 75;
+      let myNameCounter = -1;
+      let myPicWidthCounter = 25;
+      let sideBarNavInterval;
+      let myPicInterval;
+      const myNameArr = [
+          '<p>M|<p>', '<p>My|<p>', '<p>My |<p>', '<p>My n|<p>', '<p>My na|<p>','<p>My nam|<p>', '<p>My name|<p>',
+          '<p>My name |<p>','<p>My name i|<p>', '<p>My name is|<p>','<p>My name is |<p>','<p>My name is S|<p>', '<p>My name is Sa|<p>',
+          '<p>My name is Sam|<p>', '<p>My name is Same|<p>','<p>My name is Samer|<p>','<p>My name is Samer |<p>',
+          '<p>My name is Samer A|<p>','<p>My name is Samer Al|<p>', '<p>My name is Samer Ala|<p>',
+          '<p>My name is Samer Alad|<p>', '<p>My name is Samer Aladh|<p>',
+          '<p>My name is Samer Aladha|<p>', '<p>My name is Samer Aladham|<p>',
+          '<p>My name is Samer Aladhami|<p>', '<p>My name is Samer Aladhami <p>',
+      ]
+
+      $('.hello').hide()
+      $('.myPic').hide()
+      $('.sideBar-nav').hide()
+      $('.hello').fadeIn( 1000 );
+      let hello = setInterval(() => {
+        marginCounter -= 0.1
+        $('.hello').css('margin-top' , marginCounter+'px')
+      },1)
+
+      $(document).ready(() => {setTimeout(() => {
+        clearInterval( hello );
+        $('.myPic').fadeIn( 1000 );
+
+      },750)
+
+      setTimeout(() => {
+          let typeMyNmae = setInterval(()=>{
+          if(myNameCounter < myNameArr.length){
+            myNameCounter++
+          }
+            $('.myName').html(myNameArr[myNameCounter])
+          },75)
+      }, 1800)
+
+      myPicInterval = setInterval(()=>{
+        if($('.myPic').css('width') !== '247px'){
+          myPicWidthCounter++
+          $('.myPic').css('width' , myPicWidthCounter + 'px')
+
+        }
+      },0.5)
+
+      setTimeout(()=>{
+        sideBarNavInterval = setInterval(() => {
+          $('.sideBar-nav').fadeIn( 1000 );
+          if($('.sideBar-nav').css('margin-top') !== '0px'){
+            sideBarMarginCounter--
+            $('.sideBar-nav').css('margin-top' , sideBarMarginCounter+'px')
+          }
+        },10)
+      },3700)})
+
+      setTimeout(()=>{
+        clearInterval( sideBarNavInterval );
+        clearInterval( myPicInterval );
+      },5000)
+
+    }
+  }
+})
--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-angular.module('portfolio' , [])
+angular.module('portfolio' , ['ui.router'])
+  .config( function( $stateProvider, $urlRouterProvider ) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+      .state( 'home', {
+        url: '/',
+        controller: 'homeCtrl'
+        ,templateUrl: 'templates/homeTemp.html'
+
+      } )
+
+    } )
+    
