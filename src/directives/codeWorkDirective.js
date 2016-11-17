0a1a31afad3674c7bae60c37692ff547d5f8c497
samer <samaladhami@gmail.com>
2016-11-16 22:00:24 -0600

    test3

--- src/directives/codeWorkDirective.js
+++ src/directives/codeWorkDirective.js
@@ -4,15 +4,17 @@ angular.module('portfolio')
   return{
     restract: 'E'
     ,controller: 'homeCtrl'
-    ,templateUrl: '../templates/codeWorkDirective.html'
+    // ,templateUrl: '../templates/codeWorkDirective.html'
     ,link: function(scope, element, attrs) {
 
-      $('.sideBar-nav').css('margin-top', '0px');
+      function test(){$('.sideBar-nav').css('margin-top', '0px');
       $('.sideBar-nav').show();
       $('.who-I-am').css('color' , '#464646');
       $('.code-work-button').css('color' ,'rgb(193, 192, 192)');
       $('.what-I-know').css('color' , '#464646');
-      $('.contact').css('color' , '#464646');
+      $('.contact').css('color' , '#464646');}
+
+      scope.test = test
 
 
 
